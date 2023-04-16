b40ecff06ca76c8ec6bb85571d916550b688a0d4
Sergei Klimov <s.klimov@bk.ru>
2023-04-16 21:42:58 +0700

    the script of sending the file is implemented

---
+++ sender.py
@@ -0,0 +1,83 @@
+import asyncio
+import logging
+import os
+import sys
+from urllib.parse import urljoin
+
+import aiofiles
+import aiohttp
+import configargparse as configargparse
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s] %(levelname)s %(message)s",
+    datefmt="%d/%b/%Y %H:%M:%S",
+    stream=sys.stdout
+)
+logger = logging.getLogger(__name__)
+
+SAVE_FILE_HOST = 'http://localhost:8080'
+SAVE_FILE_URL = '/archive/save/'
+CHUNK_SIZE = 2 ** 16
+FILE_NAME = 'test_photos/7kna/2.jpg'
+
+
+def get_args() -> configargparse.Namespace:
+    """Получаем аргументы из командной строки"""
+
+    parser = configargparse.ArgParser(default_config_files=['.env', ], ignore_unknown_config_file_keys=True)
+
+    parser.add('--protocol', type=str, required=True, choices=['http', 'https'],
+               help='Протокол файлового сервера')
+    parser.add('--host', type=str, required=True,
+               help='Хост файлового сервера')
+    parser.add('--port', type=int, required=True,
+               help='Порт файлового сервера')
+    parser.add('--url', type=str, required=True,
+               help='Урл для доступа к методу сохранения файла')
+    parser.add('--chunk_size', type=int, required=True,
+               help='Размер порции файла для считывания в ОЗУ, в байтах')
+    parser.add('--path', type=str, required=True,
+               help='Файл для сохранения в файловом сервисе')
+
+    return parser.parse_args()
+
+
+async def file_sender(file_name: str, chunk_size: int):
+    """Генератор считывания файла по частям"""
+
+    async with aiofiles.open(file_name, 'rb') as f:
+        chunk = await f.read(chunk_size)
+
+        while chunk:
+            yield chunk
+            chunk = await f.read(chunk_size)
+
+
+async def main():
+    """Функция генерации post-запроса в адрес файлового сервиса"""
+
+    args = get_args()
+
+    url = urljoin(f'{args.protocol}://{args.host}:{args.port}', args.url)
+    headers = {
+        'CONTENT-DISPOSITION': f'attachment;filename={os.path.basename(args.path)}',
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                url,
+                headers=headers,
+                data=file_sender(file_name=FILE_NAME, chunk_size=args.chunk_size)
+        ) as resp:
+            logger.info(await resp.text())
+
+
+if __name__ == '__main__':
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
