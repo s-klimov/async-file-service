507b3edd108f8781368f221e63c20d3a209b55bc
Sergey Klimov <ksiu@pkzdrav.ru>
2023-05-04 18:13:04 +0700

    Реализован тест сохранения файла в сервисе

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,15 +1,18 @@
+import io
+import logging
 import os
 import uuid
 
 import pytest
 from aiohttp import web
+from aiologger.loggers.json import JsonLogger
 from sqlalchemy.ext.asyncio import create_async_engine
 
 import server
 from server import get_file, save_file, metadata, app
 
 TEST_FOLDER = 'archive'
-TEST_CHUNK_SIZE = 1000
+TEST_CHUNK_SIZE = 3
 
 
 @pytest.fixture
@@ -51,3 +54,25 @@ async def test_404(monkeypatch, cli, db_engine):
 
     resp = await cli.get(f'/files/{uuid.uuid4()}/')
     assert resp.status == 404
+
+
+@pytest.mark.usefixtures("setup_db")
+async def test_save_file_success(monkeypatch, cli, db_engine):
+
+    monkeypatch.setitem(app, 'folder', TEST_FOLDER)
+    monkeypatch.setitem(app, 'chunk_size', TEST_CHUNK_SIZE)
+
+    monkeypatch.setattr(server, "engine", db_engine)
+
+    data = io.BytesIO(b"some random data")
+
+    headers = {
+        # "Transfer-Encoding": "chunked",
+        'CONTENT-DISPOSITION': 'attachment;filename=random.txt'
+    }
+
+    resp = await cli.post('/files/', data=data, headers=headers, chunked=TEST_CHUNK_SIZE)
+    file_id = await resp.text()
+
+    assert resp.status == 201
+    assert uuid.UUID(file_id)
